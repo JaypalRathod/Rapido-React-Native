9e7cb29cad473403de3f8c1be8af1db6ce886076
Jaypal Rathod <jaypalrathod7147@gmail.com>
2024-12-08 14:39:11 +0530

    login screen init

--- src/app/captain/auth.tsx
+++ src/app/captain/auth.tsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { authStyles } from '@/styles/authStyles'
+import { commonStyles } from '@/styles/commonStyles'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import CustomText from '@/components/shared/CustomText';
+import PhoneInput from '@/components/shared/PhoneInput';
+import CustomButton from '@/components/shared/CustomButton';
+import { resetAndNavigate } from '@/utils/Helpers';
 
 const Auth = () => {
-  return (
-    <View>
-      <Text>Caption</Text>
-    </View>
-  )
+
+    const [phone, setPhone] = useState('');
+
+    const handleNext = async () => {
+        resetAndNavigate('/caption/home')
+    }
+
+    return (
+        <SafeAreaView style={authStyles.container}>
+            <ScrollView contentContainerStyle={authStyles.container}>
+
+                <View style={commonStyles.flexRowBetween}>
+                    <Image source={require('../../assets/images/captain_logo.png')} style={authStyles.logo} />
+                    <TouchableOpacity style={authStyles.flexRowGap}>
+                        <MaterialIcons name="help" size={18} color={'grey'} />
+                        <CustomText fontFamily='Medium' variant='h7'>Help</CustomText>
+                    </TouchableOpacity>
+                </View>
+
+                <CustomText fontFamily='Medium' variant='h6'>
+                   Good to see you, caption!
+                </CustomText>
+
+                <CustomText fontFamily='Regular' variant='h7' style={commonStyles.lightText}>
+                    Enter your phone number to proceed
+                </CustomText>
+
+                <PhoneInput
+                    onChangeText={setPhone}
+                    value={phone}
+                />
+
+            </ScrollView>
+
+            <View style={authStyles.footerContainer}>
+                <CustomText variant='h8' fontFamily='Regular' style={[commonStyles.lightText, { textAlign: 'center', marginHorizontal: 20 }]}>
+                    By continuing, you agree to the terms and conditions of Rapido
+                </CustomText>
+                <CustomButton
+                    title="Next"
+                    onPress={handleNext}
+                    loading={false}
+                    disabled={false}
+                />
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default Auth
--- src/app/customer/auth.tsx
+++ src/app/customer/auth.tsx
@@ -4,10 +4,17 @@ import { authStyles } from '@/styles/authStyles'
 import { commonStyles } from '@/styles/commonStyles'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import CustomText from '@/components/shared/CustomText';
+import PhoneInput from '@/components/shared/PhoneInput';
+import CustomButton from '@/components/shared/CustomButton';
+import { resetAndNavigate } from '@/utils/Helpers';
 
 const Auth = () => {
 
-    const [phone, setPhone] = useState('')
+    const [phone, setPhone] = useState('');
+
+    const handleNext = async () => {
+        resetAndNavigate('/customer/home')
+    }
 
     return (
         <SafeAreaView style={authStyles.container}>
@@ -25,11 +32,28 @@ const Auth = () => {
                     Whats your number ?
                 </CustomText>
 
-                <CustomText fontFamily='Regular' variant='h7'>
+                <CustomText fontFamily='Regular' variant='h7' style={commonStyles.lightText}>
                     Enter your phone number to proceed
                 </CustomText>
 
+                <PhoneInput
+                    onChangeText={setPhone}
+                    value={phone}
+                />
+
             </ScrollView>
+
+            <View style={authStyles.footerContainer}>
+                <CustomText variant='h8' fontFamily='Regular' style={[commonStyles.lightText, { textAlign: 'center', marginHorizontal: 20 }]}>
+                    By continuing, you agree to the terms and conditions of Rapido
+                </CustomText>
+                <CustomButton
+                    title="Next"
+                    onPress={handleNext}
+                    loading={false}
+                    disabled={false}
+                />
+            </View>
         </SafeAreaView>
     )
 }
